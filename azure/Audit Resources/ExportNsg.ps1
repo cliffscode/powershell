5e8e2dac9373e4428b0fef020f7f5e5e7d67a3a8
Michael Roger <michael@merlus.com>
2018-10-09 11:27:22 +1000

    Update NSG export to support multi value properties

--- azure/Audit Resources/ExportNsg.ps1
+++ azure/Audit Resources/ExportNsg.ps1
@@ -4,7 +4,7 @@ $exportPath = 'C:\temp'
 Foreach ($nsg in $nsgs) {
     $nsgRules = $nsg.SecurityRules
     foreach ($nsgRule in $nsgRules) {
-        $nsgRule | Select-Object Name,Description,Priority,SourceAddressPrefix,SourcePortRange,DestinationAddressPrefix,DestinationPortRange,Protocol,Access,Direction `
-        | Export-Csv "$exportPath\$($nsg.Name).csv" -NoTypeInformation -Encoding ASCII -Append
+        $nsgRule | Select-Object Name,Description,Priority,@{Name=’SourceAddressPrefix’;Expression={[string]::join(“,”, ($_.SourceAddressPrefix))}},@{Name=’SourcePortRange’;Expression={[string]::join(“,”, ($_.SourcePortRange))}},@{Name=’DestinationAddressPrefix’;Expression={[string]::join(“,”, ($_.DestinationAddressPrefix))}},@{Name=’DestinationPortRange’;Expression={[string]::join(“,”, ($_.DestinationPortRange))}},Protocol,Access,Direction `
+        | Export-Csv "$exportPath\$($nsg.Name).csv" -NoTypeInformation -Encoding ASCII
     }
 }
